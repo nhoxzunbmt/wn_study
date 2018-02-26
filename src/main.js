5fdeda41065a6614e187a05933806d1500274527
nhoxzunbmt <nhoxzunbmt@users.noreply.github.com>
2018-02-26 17:43:40 +0700

    Update

--- src/main.js
+++ src/main.js
@@ -11,6 +11,15 @@ import './assets/css/style.css'
 import './assets/css/font-awesome.css'
 import './assets/css/easy-responsive-tabs.css'
 
+// import './assets/js/jquery-2.1.4.min.js'
+// import './assets/js/modernizr.custom.js'
+// import './assets/js/minicart.min.js'
+// import './assets/js/easy-responsive-tabs.js'
+// import './assets/js/jquery.waypoints.min.js'
+// import './assets/js/jquery.countup.js'
+// import './assets/js/move-top.js'
+// import './assets/js/jquery.easing.min.js'
+// import './assets/js/bootstrap.js'
 
 /* eslint-disable no-new */
 new Vue({
